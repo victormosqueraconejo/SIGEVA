545d482ce30f898bb39068dc74fb010c57eb02c4
Bryanhurtado0006 <bryandreshurtado18@gmail.com>
2025-09-09 14:18:05 -0500

    subida de cambios para un posterior PR

--- app/src/main/java/com/victor/sigeva/LoginActivity.kt
+++ app/src/main/java/com/victor/sigeva/LoginActivity.kt
@@ -1,20 +1,49 @@
 package com.victor.sigeva
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val email = findViewById<EditText>(R.id.input_email)
+        val password = findViewById<EditText>(R.id.input_password)
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+
+        btnLogin.setOnClickListener {
+            val url = "http://tu-servidor.com/api/login"
+
+            val queue = Volley.newRequestQueue(this)
+
+            val request = object : StringRequest(
+                Request.Method.POST, url,
+                { response ->
+                    //  respuesta del backend
+                    Toast.makeText(this, "Respuesta: $response", Toast.LENGTH_LONG).show()
+                },
+                { error ->
+                    // Si ocurre un error
+                    Toast.makeText(this, "Error: Completa los campos para ingresar", Toast.LENGTH_LONG).show()
+                }
+            ) {
+                override fun getParams(): MutableMap<String, String> {
+                    val params = HashMap<String, String>()
+                    params["email"] = email.text.toString()
+                    params["password"] = password.text.toString()
+                    return params
+                }
+            }
+
+            queue.add(request)
         }
     }
 }
